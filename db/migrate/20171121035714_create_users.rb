bcddc8044e8e82a2f3c63d04b4b510ca68c6d105
Ashe Austaire <AsheAustaire@gmail.com>
2017-11-21 11:28:11 -0500

    add_user

--- db/migrate/20171121035714_create_users.rb
+++ db/migrate/20171121035714_create_users.rb
@@ -1,17 +1,47 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string :name
+      #User Data Points
+      t.string :first_name
+      t.string :middle_name
+      t.string :last_name
       t.string :email
-      t.string :password_digetst
-      t.integer :age
-      t.string :gender
-      t.string :current_city
-      t.string :current_state
-      t.string :ethnicity
-      t.string :nationality
 
       t.timestamps
     end
   end
 end
+
+
+# #User Ideas
+# t.integer :idea_id
+#
+# #User Spaces
+# t.integer :
+
+# #ID's and CUID
+
+# t.string :password_digest
+# t.integer :age
+# t.string :generation
+# t.string :gender
+# t.string :sex
+# t.string :language
+# t.string :current_city
+# t.string :current_state
+# t.string :ethnicity
+# t.string :nationality
+# t.integer :CUID
+# t.string :family
+# t.string :books
+# t.string :places_lived
+# t.string :nickname
+# t.string :musical_interests
+# t.string :sexual_bias
+# t.string :school
+# t.string :field_of_study
+# t.string :profession
+# t.string :politics
+# t.string :relationship
+# t.string :religion
+# t.string :interests
